57a52d631e0c708b8e3a071e8f45b9b6250b6993
Miguel Gonzalez Castro <mgonzcas@everis.com>
2018-11-17 21:49:32 +0100

    Configurando routes

--- seccion_4/spa/src/app/components/heroes/heroes.component.ts
+++ seccion_4/spa/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private _heroesService: HeroesService, private router: Router) {}
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
+  verHeroe(id: number) {
+    console.log(id);
+    this.router.navigate(['/heroe', id]);
+  }
+}
+
+export interface Heroe {
+  nombre: string;
+  bio: string;
+  img: string;
+  aparicion: string;
+  casa: string;
 }
